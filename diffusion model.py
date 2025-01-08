011b915bf893fa76eaf93c1344eaaedee475c23e
Parvathi <parvathinair.17@gmail.com>
2025-01-08 21:17:09 +0000

    Remove all comments from script

--- diffusion model.py
+++ diffusion model.py
@@ -13,97 +13,26 @@
 # 
 # The discretized version of the diffusion equation that we'll solve with our model:
 # $$ C^{t+1}_x = C^t_x + {D \Delta t \over \Delta x^2} (C^t_{x+1} - 2C^t_x + C^t_{x-1}) $$
-# 
-# This is the explicit FTCS scheme as described in Slingerland and Kump (2011). (or see wikipedia)
-# 
-
-# We will use two libraries, Numpy (for arrays)
-# and Matplotlib (for plotting)
-# that are not the part of the base Python distribution
-
-# In[ ]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# Set two fixed model parameters, the diffusivity and the size of the model domain
-
-# In[ ]:
 
 
 D = 100 #diffusivity
 Lx = 300 #domain size
 
 
-# Next, set up the model grid using NumPy array.
-
-# In[ ]:
-
 
 dx = 0.5
 x = np.arange(start=0, stop=Lx, step=dx)
 nx= len(x)
-
-
-# In[ ]:
-
-
-whos
-
-
-# In[ ]:
-
-
-x
-
-
-# In[ ]:
-
-
-x[0]
-
-
-# In[ ]:
-
-
-x[nx-1]
-
-
-# In[ ]:
-
-
-x[-1]
-
-
-# In[ ]:
-
-
-x[0:5]
-
-
-# Set the initial concentration profile for the model.
-# The concentration 'C' is a step function
-# with a high value on the left,
-# a low value on the right,
-# and the step ar rge center of the domain
-
-# Set the initial concentration profile for the model. The concentration `C` is a step function with a high value on the left, a low value on the right, and the step ar rge center of the domain
-
-# In[ ]:
-
 
 C=np.zeros_like(x)
 C_left=500
 C_right=0
 C[x<=Lx//2]=C_left
 C[x>Lx//2]=C_right
-
-
-# Plot the initial profile
-
-# In[ ]:
 
 
 plt.figure()
@@ -113,39 +42,14 @@ plt.ylabel("C")
 plt.title("initial concentration profile")
 
 
-# Set the start time of the model and the number of time steps.
-# Calculate a stable time step for the model using a stability criterion.
-
-# In[ ]:
-
-
 time=0
 nt=5000
 dt=0.5*(dx**2/D)
-
-
-# In[ ]:
-
-
-dt
-
-
-# Loop over the time steps of the model,
-# solving the diffusion equation using the FTCS explicit scheme described above.
-# The boundary conditions are fixed, so reset them at each time step.
-
-# In[ ]:
-
 
 for t in range(0,nt):
     C+=D*dt/dx**2*(np.roll(C,-1)-2*C+np.roll(C,1))
     C[0]=C_left
     C[-1]=C_right
-
-
-# Plot the result.
-
-# In[ ]:
 
 
 plt.figure()
