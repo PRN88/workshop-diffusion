a1f811b775f189406a07c0517c7961660bed5fbe
Parvathi <parvathinair.17@gmail.com>
2025-01-09 17:41:59 +0000

    set up sample main program

--- diffusion.py
+++ diffusion.py
@@ -14,31 +14,44 @@ def make_grid(origin,domain_size,grid_spacing):
     grid=np.arange(start=origin,stop=origin+domain_size,step=grid_spacing)
     return(grid,len(grid))
     
-def plot_profile(grid, concentartion, color="r", title="Concentration profile"):
+def plot_profile(grid, concentration, color="r", title="Concentration profile"):
     plt.figure()
-    plt.plot(grid, concentartion,color)
+    plt.plot(grid, concentration,color)
     plt.xlabel("x")
     plt.ylabel("C")
     plt.title(title)
 
-def solve_1d_diffusion(concentartion,grid_spacing=1.0,time_step=1.0,diffusivity=1.0):
-    centered_difference=np.roll(concentartion,-1)-2*concentartion+np.roll(concentartion,1)
+def solve_1d_diffusion(concentration,grid_spacing=1.0,time_step=1.0,diffusivity=1.0):
+    centered_difference=np.roll(concentration,-1)-2*concentration+np.roll(concentration,1)
     concentration[1:-1]+=diffusivity*time_step/grid_spacing**2*centered_difference[1:-1]
     
-def diffusin_model():
+def diffusion_model(plot=False,domain_size=300,n_time_steps=5000):
     D = 100 #diffusivity
-    Lx = 300 #domain size
+    Lx = domain_size #domain size
     dx = 0.5
     C_left=500
     C_right=0
-    nt=5000
+    nt=n_time_steps
     
     x, nx= make_grid(0,Lx,dx)
     dt=calculate_time_step(dx,D)
     C=set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
-    plot_profile(x, C, title="Initial concentration profile")
     
+    if plot is True:
+        plot_profile(x, C, title="Initial concentration profile")
+    
+    if plot is False:
+        print("Time=0/n",C)
     for t in range(0,nt):
-    solve_1d_diffusion(C,dx,dt,D)
+        solve_1d_diffusion(C,dx,dt,D) 
+
+    if plot is False:
+        print(f"Time={t*dt}/n",C)
     
-    plot_profile(x, C, color="b", title="final concentration profile")
+    if plot is True:
+        plot_profile(x, C, color="b", title="final concentration profile")
+
+
+if  __name__=="__main__":
+    print("Diffusion model")
+    diffusion_model(plot=False, domain_size=5,n_time_steps=5)
